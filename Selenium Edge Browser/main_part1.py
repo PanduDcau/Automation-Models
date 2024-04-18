01ab12472e5784fbf7df36bb21d528fa90290ade
W. Pandula <52339029+PanduDcau@users.noreply.github.com>
2024-04-18 23:23:17 +0530

    Update main_part1.py

--- Selenium Edge Browser/main_part1.py
+++ Selenium Edge Browser/main_part1.py
@@ -1,8 +1,10 @@
 # importing required package of webdriver
 from selenium import webdriver
+
 # Just Run this to execute the below script
 if __name__ == '__main__':
-   # Instantiate the webdriver with the executable location of MS Edge web driver
+   # Instantiate the webdriver with the executable location of MS Edge web driver for Execution
    browser = webdriver.Edge(r"C:\Users\LenovoE14\Downloads\edgedriver\msedgedriver.exe")
+   
    # Simply just open a new Edge browser and go to lambdatest.com
    browser.get('https://www.lambdatest.com')
