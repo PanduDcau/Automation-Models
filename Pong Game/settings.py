5384a68b8d394f27fcbe9bd9a25dc31d57242821
W. Pandula <52339029+PanduDcau@users.noreply.github.com>
2024-05-29 23:42:08 +0530

    Update settings.py

--- Pong Game/settings.py
+++ Pong Game/settings.py
@@ -1,4 +1,5 @@
-#This section provide the code variables for the pong game Applications
+#This section provide the code variables for the pong game application
+#Height and Width will be reallocated
 WIDTH, HEIGHT = 990, 450
 
 player_width, player_height  = 20, 90
